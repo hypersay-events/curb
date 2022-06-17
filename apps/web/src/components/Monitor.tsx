08810d7a54333a673e12998326d7ed503d095426
Cristian Dinu <cristian.dinu@hypersay.com>
2022-06-17 11:08:59 +0100

    Web Interface Endpoint is configurable

--- apps/web/src/components/Monitor.tsx
+++ apps/web/src/components/Monitor.tsx
@@ -10,6 +10,9 @@ interface MonitorProps {
   language?: string;
 }
 
+const NEXT_PUBLIC_CAPTIONS_ENDPOINT =
+  process.env.NEXT_PUBLIC_CAPTIONS_ENDPOINT || "http://localhost:4664";
+
 export const Monitor: React.FC<MonitorProps> = ({
   roomName,
   language = "original",
