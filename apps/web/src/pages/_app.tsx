9bb26d4ca440acf8df5c6895b3d8b5d0a6198397
Paul Balogh <paul.balogh@gmail.com>
2022-06-21 13:59:53 +0100

    Use footer for captioners

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -21,48 +21,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       withGlobalStyles
       withNormalizeCSS
     >
-      <AppShell
-        fixed
-        header={
-          router.query.room ? (
-            <Header height={50} p={6}>
-              <Button
-                onClick={() => {
-                  router.back();
-                }}
-                leftIcon={<IconArrowNarrowLeft />}
-                size="sm"
-                color="gray"
-                variant="subtle"
-              >
-                another room
-              </Button>
-            </Header>
-          ) : undefined
-        }
-        footer={
-          <Footer
-            height={60}
-            p="md"
-            style={{ display: "flex", justifyContent: "flex-end" }}
-          >
-            <Text color="dimmed">
-              created by{" "}
-              <Anchor
-                href="https://hypersay.events"
-                target="hypersay_events"
-                color="gray"
-              >
-                Hypersay Events
-              </Anchor>
-            </Text>
-          </Footer>
-        }
-      >
-        <Center style={{ width: "100%", height: "100%" }}>
-          <Component {...pageProps} />
-        </Center>
-      </AppShell>
+      <Component {...pageProps} />
     </MantineProvider>
   );
 }
