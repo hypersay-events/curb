2d5f71b52802ac1ffcaba62c81c5d81ea4eb5ea1
Paul Balogh <paul.balogh@gmail.com>
2022-06-23 16:14:56 +0100

    Remake basic settings theme

--- apps/native/src/components/Settings.tsx
+++ apps/native/src/components/Settings.tsx
@@ -1,19 +1,27 @@
 import {
   Button,
   Center,
+  Chip,
+  Chips,
   ColorInput,
   Group,
   Paper,
+  ScrollArea,
   Slider,
   Stack,
   Text,
   useMantineTheme,
 } from "@mantine/core";
 import { MarksProps } from "@mantine/core/lib/components/Slider/Marks/Marks";
-import { useState } from "react";
+import { SetStateAction, useState } from "react";
 import { FIRST_LINERS } from "./Captions";
 import { appWindow } from "@tauri-apps/api/window";
-import { DEFAULT, useCaptionsTheme } from "../hooks/useCaptionsTheme";
+import {
+  CaptionStylePresets,
+  CAPTION_STYLES,
+  useCaptionsTheme,
+} from "../hooks/useCaptionsTheme";
+import CaptionLine from "./CaptionLine";
 
 export const STEP = 0.1;
 
@@ -26,14 +34,11 @@ export const Settings = () => {
   const theme = useMantineTheme();
   const {
     captionsTheme,
-    setTextColor,
+    setCaptionStyle,
     setFontSize,
-    setTextBackground,
-    setTextShadow,
-    setTextStroke,
     setTextWeight,
-    setBionicReading,
     setLineHeight,
+    setBionicReading,
     setMode,
     setWindowBackground,
   } = useCaptionsTheme();
@@ -49,69 +54,110 @@ export const Settings = () => {
     { value: 5, label: "massive" },
   ];
 
+  const FONTWEIGHT_MARKS: MarksProps["marks"] = [
+    { value: 200, label: "thin" },
+    { value: 300, label: "normal" },
+    { value: 500, label: "bold" },
+    { value: 700, label: "bolder" },
+    { value: 900, label: "black" },
+  ];
+
+  const swatches = [
+    theme.fn.rgba("#ffffff", 1),
+    theme.fn.rgba("#868e96", 1),
+    theme.fn.rgba("#25262b", 1),
+    theme.fn.rgba("#000000", 1),
+    theme.fn.rgba("#fa5252", 1),
+    theme.fn.rgba("#e64980", 1),
+    theme.fn.rgba("#be4bdb", 1),
+    theme.fn.rgba("#7950f2", 1),
+    theme.fn.rgba("#4c6ef5", 1),
+    theme.fn.rgba("#228be6", 1),
+    theme.fn.rgba("#15aabf", 1),
+    theme.fn.rgba("#12b886", 1),
+    theme.fn.rgba("#40c057", 1),
+    theme.fn.rgba("#82c91e", 1),
+    theme.fn.rgba("#fab005", 1),
+    theme.fn.rgba("#fd7e14", 1),
+  ];
+
+  console.log({ captionsTheme });
+
   return (
     <Paper
       style={{
         height: "100vh",
         width: "100vw",
+        overflow: "auto",
       }}
       p="lg"
     >
-      <Stack spacing="xl" align="start" style={{ height: "100%" }}>
+      <Stack spacing="xl" align="start">
         <Text size="lg" color="white">
           Settings
         </Text>
 
-        <Group>
-          <ColorInput
-            label="Text color"
-            value={captionsTheme.TextColor}
-            onChange={setTextColor}
-            format="rgba"
-            swatches={[
-              theme.fn.rgba("#ffffff", 1),
-              theme.fn.rgba("#868e96", 1),
-              theme.fn.rgba("#25262b", 1),
-              theme.fn.rgba("#000000", 1),
-              theme.fn.rgba("#fa5252", 1),
-              theme.fn.rgba("#e64980", 1),
-              theme.fn.rgba("#be4bdb", 1),
-              theme.fn.rgba("#7950f2", 1),
-              theme.fn.rgba("#4c6ef5", 1),
-              theme.fn.rgba("#228be6", 1),
-              theme.fn.rgba("#15aabf", 1),
-              theme.fn.rgba("#12b886", 1),
-              theme.fn.rgba("#40c057", 1),
-              theme.fn.rgba("#82c91e", 1),
-              theme.fn.rgba("#fab005", 1),
-              theme.fn.rgba("#fd7e14", 1),
-            ]}
-          />
-          <ColorInput
-            label="Background color"
-            value={captionsTheme.TextBackground}
-            onChange={setTextBackground}
-            format="rgba"
-            swatches={[
-              theme.fn.rgba("#ffffff", 1),
-              theme.fn.rgba("#868e96", 1),
-              theme.fn.rgba("#25262b", 1),
-              theme.fn.rgba("#000000", 1),
-              theme.fn.rgba("#fa5252", 1),
-              theme.fn.rgba("#e64980", 1),
-              theme.fn.rgba("#be4bdb", 1),
-              theme.fn.rgba("#7950f2", 1),
-              theme.fn.rgba("#4c6ef5", 1),
-              theme.fn.rgba("#228be6", 1),
-              theme.fn.rgba("#15aabf", 1),
-              theme.fn.rgba("#12b886", 1),
-              theme.fn.rgba("#40c057", 1),
-              theme.fn.rgba("#82c91e", 1),
-              theme.fn.rgba("#fab005", 1),
-              theme.fn.rgba("#fd7e14", 1),
-            ]}
-          />
+        <Text>Caption Styles</Text>
+
+        <Group spacing={10}>
+          {CAPTION_STYLES.map((k) => {
+            const capTheme = CAPTION_STYLES.find(
+              (i) => i.StyleId === k.StyleId
+            );
+            return (
+              <Chip
+                key={k.StyleId}
+                value={k.StyleId}
+                checked={k.StyleId === captionsTheme.StyleId}
+                variant={
+                  capTheme?.StyleId === "noBackground" ? "outline" : "filled"
+                }
+                color="gray"
+                styles={{
+                  iconWrapper: { color: capTheme?.TextColor },
+                  checked: {
+                    color: capTheme?.TextColor,
+                    background: `${capTheme?.TextBackground} !important`,
+                  },
+                  filled: {
+                    color: capTheme?.TextColor,
+                    background: capTheme?.TextBackground,
+                    fontWeight: "bold",
+                  },
+                  outline: {
+                    fontWeight: "bold",
+                  },
+                }}
+                onChange={() =>
+                  setCaptionStyle({
+                    ...capTheme,
+                  })
+                }
+              >
+                {k.StyleLabel}
+              </Chip>
+            );
+          })}
         </Group>
+
+        {captionsTheme.StyleId === "custom" ? (
+          <Group>
+            <ColorInput
+              label="Text color"
+              value={captionsTheme.TextColor}
+              onChange={(e) => setCaptionStyle({ TextColor: e })}
+              format="rgba"
+              swatches={swatches}
+            />
+            <ColorInput
+              label="Background color"
+              value={captionsTheme.TextBackground}
+              onChange={(e) => setCaptionStyle({ TextBackground: e })}
+              format="rgba"
+              swatches={swatches}
+            />
+          </Group>
+        ) : null}
 
         <Group style={{ width: 400 }}>
           <Text size="sm">Font size</Text>
@@ -131,23 +177,33 @@ export const Settings = () => {
             marks={FONTSIZE_MARKS}
           />
         </Group>
-        <Center style={{ flexGrow: 1 }}>
-          <Text
-            component="span"
-            style={{
-              display: "inline",
-              fontSize: `${captionsTheme.FontSize}vw`,
-              fontWeight: "bold",
-              lineHeight: "110%",
-              backgroundColor: captionsTheme.TextBackground,
-              boxShadow: `0.2em 0 0 ${captionsTheme.TextBackground},-0.2em 0 0 ${captionsTheme.TextBackground}`,
-              color: captionsTheme.TextColor,
-              transition: "color 0.5s ease",
-              // borderRadius: theme.radius.md,
-            }}
-          >
-            {previewLine}
-          </Text>
+        {/* <Group style={{ width: 400 }}>
+          <Text size="sm">Font weight</Text>
+          <Slider
+            style={{ flexGrow: 1 }}
+            // labelAlwaysOn
+            defaultValue={captionsTheme.FontSize}
+            min={2}
+            max={5}
+            label={(fontSize) =>
+              FONTSIZE_MARKS.find((mark) => mark.value === fontSize)?.label ||
+              `${fontSize.toFixed(1)}vw`
+            }
+            step={STEP}
+            styles={{ markLabel: { display: "none" } }}
+            onChange={setTextWeight}
+            marks={FONTWEIGHT_MARKS}
+          />
+        </Group> */}
+        <Center
+          style={{
+            flexGrow: 1,
+            border: `1px solid ${theme.colors.gray[7]}`,
+            borderRadius: theme.radius.lg,
+          }}
+          p="lg"
+        >
+          <CaptionLine text={previewLine} />
         </Center>
 
         {/* <Switch
@@ -155,7 +211,7 @@ export const Settings = () => {
           checked={showOnTop}
           onChange={(event) => setShowOnTop(event.currentTarget.checked)}
         /> */}
-        <Button onClick={() => appWindow.close()}>Close settings</Button>
+        <Button onClick={() => appWindow.close()}>Done</Button>
       </Stack>
     </Paper>
   );
--- apps/native/src/hooks/useCaptionsTheme.ts
+++ apps/native/src/hooks/useCaptionsTheme.ts
@@ -1,12 +1,26 @@
 import { MantineColor } from "@mantine/core";
 import { useLocalStorage } from "@mantine/hooks";
 
-export type CaptionsTheme = {
-  FontSize: number;
+export type CaptionStylePresets =
+  | "whiteBlack"
+  | "yellowBlack"
+  | "blackYellow"
+  | "noBackground"
+  | "whiteBlue"
+  | "yellowBlue"
+  | "custom";
+
+export type CaptionStyle = {
+  StyleId: CaptionStylePresets;
+  StyleLabel: string;
   TextColor: MantineColor;
   TextBackground: MantineColor;
   TextShadow: string;
   TextStroke: string;
+};
+
+export type CaptionsTheme = CaptionStyle & {
+  FontSize: number;
   TextWeight: "thin" | "normal" | "bold" | "bolder" | "black";
   LineHeight: 1 | 1.5 | 2;
   WindowBackground: MantineColor;
@@ -14,12 +28,73 @@ export type CaptionsTheme = {
   Mode: "cc" | "transcript";
 };
 
+export const CAPTION_STYLES: CaptionStyle[] = [
+  {
+    StyleId: "whiteBlack",
+    StyleLabel: "White / Black",
+    TextColor: "rgba(255, 255, 255, 1)",
+    TextBackground: "rgba(0, 0, 0, 1)",
+    TextShadow: "transparent",
+    TextStroke: "",
+  },
+  {
+    StyleId: "yellowBlack",
+    StyleLabel: "Yellow / Black",
+    TextColor: "rgba(255, 225, 0, 1)",
+    TextBackground: "rgba(0, 0, 0, 1)",
+    TextShadow: "transparent",
+    TextStroke: "",
+  },
+  {
+    StyleId: "blackYellow",
+    StyleLabel: "Black / Yellow",
+    TextColor: "rgba(0, 0, 0, 1)",
+    TextBackground: "rgba(255, 225, 0, 1)",
+    TextShadow: "transparent",
+    TextStroke: "",
+  },
+  {
+    StyleId: "whiteBlue",
+    StyleLabel: "White / Blue",
+    TextColor: "rgba(255, 255, 255, 1)",
+    TextBackground: "rgba(56, 65, 224, 1)",
+    TextShadow: "transparent",
+    TextStroke: "",
+  },
+  {
+    StyleId: "yellowBlue",
+    StyleLabel: "Yellow / Blue",
+    TextColor: "rgba(255, 225, 0, 1)",
+    TextBackground: "rgba(56, 65, 224, 1)",
+    TextShadow: "transparent",
+    TextStroke: "",
+  },
+  {
+    StyleId: "noBackground",
+    StyleLabel: "No Background",
+    TextColor: "rgba(255, 255, 255, 1)",
+    TextBackground: "transparent",
+    TextShadow: "rgba(0,0,0,.5)",
+    TextStroke: "",
+  },
+  {
+    StyleId: "custom",
+    StyleLabel: "Custom",
+    TextColor: "rgba(255, 255, 255, 1)",
+    TextBackground: "rgba(0, 0, 0, 1)",
+    TextShadow: "transparent",
+    TextStroke: "",
+  },
+];
+
 export const DEFAULT: CaptionsTheme = {
-  FontSize: 3,
+  StyleId: "whiteBlack",
+  StyleLabel: "White / Black",
   TextColor: "rgba(255, 255, 255, 1)",
   TextBackground: "rgba(0, 0, 0, 1)",
   TextShadow: "",
   TextStroke: "",
+  FontSize: 3,
   TextWeight: "bold",
   LineHeight: 1,
   WindowBackground: "transparent",
@@ -31,6 +106,16 @@ export const useCaptionsTheme = () => {
   const [fontSize, setFontSize] = useLocalStorage({
     key: "font-size",
     defaultValue: DEFAULT.FontSize,
+  });
+
+  const [styleId, setStyleId] = useLocalStorage({
+    key: "style-id",
+    defaultValue: DEFAULT.StyleId,
+  });
+
+  const [styleLabel, setStyleLabel] = useLocalStorage({
+    key: "style-label",
+    defaultValue: DEFAULT.StyleLabel,
   });
 
   const [textColor, setTextColor] = useLocalStorage({
@@ -78,12 +163,24 @@ export const useCaptionsTheme = () => {
     defaultValue: DEFAULT.Mode,
   });
 
+  const setCaptionStyle = (newStyle: Partial<CaptionStyle>) => {
+    console.log({ newStyle });
+    if (newStyle.StyleId) setStyleId(newStyle.StyleId);
+    if (newStyle.StyleLabel) setStyleLabel(newStyle.StyleLabel);
+    if (newStyle.TextColor) setTextColor(newStyle.TextColor);
+    if (newStyle.TextBackground) setTextBackground(newStyle.TextBackground);
+    if (newStyle.TextShadow) setTextShadow(newStyle.TextShadow);
+    if (newStyle.TextStroke) setTextStroke(newStyle.TextStroke);
+  };
+
   const OVERRIDE: Partial<CaptionsTheme> = {
-    FontSize: fontSize,
+    StyleId: styleId,
+    StyleLabel: styleLabel,
     TextColor: textColor,
     TextBackground: textBackground,
     TextStroke: textStroke,
     TextShadow: textShadow,
+    FontSize: fontSize,
     TextWeight: textWeight,
     LineHeight: lineHeight,
     WindowBackground: windowBackground,
@@ -95,6 +192,9 @@ export const useCaptionsTheme = () => {
 
   return {
     captionsTheme,
+    setCaptionStyle,
+    setStyleId,
+    setStyleLabel,
     setFontSize,
     setTextColor,
     setTextBackground,
