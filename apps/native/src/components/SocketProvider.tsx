1816073b34663d1fb95300a6329e0293421642f4
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2023-02-08 12:38:20 +0100

    Fallback to production server if not specified

--- apps/native/src/components/SocketProvider.tsx
+++ apps/native/src/components/SocketProvider.tsx
@@ -11,7 +11,7 @@ export const SocketContext = createContext<{
 });
 
 const VITE_CAPTIONS_ENDPOINT =
-  import.meta.env.VITE_CAPTIONS_ENDPOINT || "http://localhost:4554";
+  import.meta.env.VITE_CAPTIONS_ENDPOINT || "https://captions.hypersay.com";
 
 export const SocketProvider: React.FC<{
   children: ReactNode;
